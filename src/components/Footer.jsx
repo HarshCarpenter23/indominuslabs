798741cb894daaf27d8683b26592651e7698e5f8
Harsh Carpenter <harshcarpenter06902@gmail.com>
2025-03-13 02:44:46 +0530

    Product Animation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -37,7 +37,7 @@ const Footer = () => {
                   <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
                 </svg>
               </a>
-              <a href="https://github.com/indominus-labs" target="__blank" className="text-gray-300 hover:text-purple-600">
+              <a href="https://github.com/indominus-labs/" target="__blank" className="text-gray-300 hover:text-purple-600">
                 <svg
                   className="h-5 w-5"
                   fill="currentColor"
@@ -105,7 +105,7 @@ const Footer = () => {
           </div>
 
           {/* Company Column */}
-          <div className="md:col-span-1">
+          {/* <div className="md:col-span-1">
             <h3 className="font-semibold text-lg mb-4">Company</h3>
             <ul className="space-y-2">
               <li>
@@ -129,7 +129,7 @@ const Footer = () => {
                 </a>
               </li>
             </ul>
-          </div>
+          </div> */}
 
           {/* Contact Column */}
           <div id='contact' className="md:col-span-1">
@@ -165,7 +165,7 @@ const Footer = () => {
                     d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                   />
                 </svg>
-                contact@indominuslabs.in
+                muthuvenkatesh.murugan@gmail.om
               </li>
               <li className="flex items-center gap-2 text-gray-300">
                 <svg
